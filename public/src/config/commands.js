0dd756127d286f0597bff82815134f1604954efe
computationalorganizationalresearch <132207859+computationalorganizationalresearch@users.noreply.github.com>
2026-03-31 19:36:01 -0400

    Delete public directory

--- public/src/config/commands.js
+++
@@ -1,5 +0,0 @@
-export const COMMANDS = ["sit", "lay down", "shake paw", "neutral"];
-
-export function defaultMapping() {
-  return { w: "sit", a: "lay down", s: "shake paw", d: "neutral" };
-}
--- public/src/core/speech.js
+++
@@ -1,8 +0,0 @@
-export class Speech {
-  say(text) {
-    const utter = new SpeechSynthesisUtterance(text);
-    utter.rate = 1;
-    utter.pitch = 1;
-    speechSynthesis.speak(utter);
-  }
-}
